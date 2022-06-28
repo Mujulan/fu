446a93332166129d5fbe0a4c5fee7747c3677129
Mujulan <105826355+Mujulan@users.noreply.github.com>
2022-06-28 17:35:19 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -2505,6 +2505,16 @@ headerType: 4
 kagura.sendMessage(m.chat, buttonMessage, { quoted: m })
 }
 break
+case 'hekel': {
+let buttonMessage = {
+image: { url: 'https://betarest.herokuapp.com/api/wallpaper/hekel?apikey=APIKEY' },
+caption: ` Random ${command}`,
+footer: kagura.user.name,
+headerType: 4
+}
+kagura.sendMessage(m.chat, buttonMessage, { quoted: m })
+}
+break
 case 'onepiece': {
 let buttonMessage = {
 image: { url: 'https://betabotz-api.herokuapp.com/api/wallpaper/onepiece?apikey=BetaBotz' },
